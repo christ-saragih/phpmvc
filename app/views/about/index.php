a2c679deacca977b3a7b26fad45225247d9069a7
bennefit <csbennefit@apps.ipb.ac.id>
2023-12-06 08:46:37 +0700

    assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
+<div class="container mt-4">
     <h1>About me</h1>
+    <img src="<?= BASEURL; ?>/img/bennefit.png" alt="bennefit cs" width="200" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data["nama"]; ?>. Saya berusia <?= $data["umur"] ?> dan sekarang bekerja sebagai <?= $data["pekerjaan"] ?>. </p>
+</div>
