3a493ce38f73dca33e24c8cfaa92244aeac15b38
bennefit <csbennefit@apps.ipb.ac.id>
2023-12-06 07:40:14 +0700

    view

---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+
+    <h1>About me</h1>
+    <p>Halo, nama saya <?= $data["nama"]; ?>. Saya berusia <?= $data["umur"] ?> dan sekarang bekerja sebagai <?= $data["pekerjaan"] ?>. </p>
---
+++ app/views/home/index.php
@@ -0,0 +1,2 @@
+
+    <h1>Selamat datang gaes!</h1>
